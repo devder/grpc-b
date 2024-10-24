935cc5049033495e0f4f439f273383742df96288
hal <72111003+devder@users.noreply.github.com>
2024-10-24 10:34:24 +0100

    create accounts endpoint

---
+++ api/account.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/devder/grpc-b/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=EUR USD CAD"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Balance:  0,
+		Owner:    req.Owner,
+		Currency: req.Currency,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, account)
+}
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/devder/grpc-b/api"
+	db "github.com/devder/grpc-b/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+// update to env
+const (
+	driverName     = "postgres"
+	dataSourceName = "postgresql://root:password@localhost:5432/grpc?sslmode=disable"
+	serverAddress  = "0.0.0.0:8080"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	conn, err := sql.Open(driverName, dataSourceName)
+
+	if err != nil {
+		log.Fatal("failed to connect to DB:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
