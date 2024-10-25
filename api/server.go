a65c398b4b5c7414fbf4a6c606569c356f7ee9fb
hal <72111003+devder@users.noreply.github.com>
2024-10-25 09:33:04 +0100

    create transfers route

--- api/server.go
+++ api/server.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	db "github.com/devder/grpc-b/db/sqlc"
+	"github.com/devder/grpc-b/util"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -11,16 +14,20 @@ type Server struct {
 }
 
 func NewServer(store db.Store) *Server {
-	server := &Server{
-		store: store,
-	}
-
+	server := &Server{store: store}
 	router := gin.Default()
+
+	// register custom validator
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", util.ValidCurrency)
+	}
 
 	// add routes to router
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.getAccounts)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
