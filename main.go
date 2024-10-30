d585d9fbc607c2baa49f1d90c15dde01ff62aad9
hal <72111003+devder@users.noreply.github.com>
2024-10-30 12:26:10 +0100

    run Gateway Server

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/devder/grpc-b/api"
 	db "github.com/devder/grpc-b/db/sqlc"
 	"github.com/devder/grpc-b/gapi"
 	"github.com/devder/grpc-b/pb"
 	"github.com/devder/grpc-b/util"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -28,6 +32,7 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	go runGatewayServer(config, store) // run in a separate go routine to avoid blocking the grpc server
 	runGRPCServer(config, store)
 }
 
@@ -63,5 +68,45 @@ func runGRPCServer(config util.Config, store db.Store) {
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	// keep responses as defined in .proto (snake_case)
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterGrpcAppHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatalf("cannot register handler server: %v", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	lis, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Printf("start HTTP gateway server at %s", lis.Addr().String())
+
+	if err := http.Serve(lis, mux); err != nil {
+		log.Fatalf("failed to serve HTTP gateway server: %v", err)
 	}
 }
