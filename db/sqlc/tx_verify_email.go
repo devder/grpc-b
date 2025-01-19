4ffd5e4daed1ec6a360aaf005e9642547d0bf96e
hal <72111003+devder@users.noreply.github.com>
2025-01-19 22:52:35 +0100

    verify email and update user in tx

---
+++ db/sqlc/tx_verify_email.go
@@ -0,0 +1,46 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+)
+
+// contains the input parameters of the transfer transaction
+type VerifyEmailTxParams struct {
+	EmailId    int64
+	SecretCode string
+}
+
+// result of the transfer tx
+type VerifyEmailTxResult struct {
+	User        User
+	VerifyEmail VerifyEmail
+}
+
+// performs a money transfer from one account to another
+func (store *SQLStore) VerifyEmailTx(ctx context.Context, arg VerifyEmailTxParams) (VerifyEmailTxResult, error) {
+	var result VerifyEmailTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.VerifyEmail, err = q.UpdateVerifyEmail(ctx, UpdateVerifyEmailParams{
+			ID:         arg.EmailId,
+			SecretCode: arg.SecretCode,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.User, err = q.UpdateUser(ctx, UpdateUserParams{
+			Username: result.VerifyEmail.Email,
+			IsEmailVerified: sql.NullBool{
+				Valid: true,
+				Bool:  true,
+			}})
+
+		return err
+	})
+
+	return result, err
+}
