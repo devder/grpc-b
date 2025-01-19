80a08941ed0cdb9e4e16ddb3e42795dfe8285b2c
hal <72111003+devder@users.noreply.github.com>
2025-01-19 15:30:59 +0100

    write test for send email

---
+++ mail/sender_test.go
@@ -0,0 +1,25 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/devder/grpc-b/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+	subject := "A test email"
+	content := `
+		<h1>Hello world </h1>
+		<p>This is a test message from your Go application.</p>
+	`
+
+	to := []string{"derick@eukapay.com"}
+	attachFiles := []string{"../README.md"}
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
